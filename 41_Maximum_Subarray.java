ae5647ac4aaf0678061aaaade3b7408e6c6287e0
Alex Guo <qiuyuguo@usc.edu>
2016-10-27 07:33:46 -0700

    Update 41_Maximum_Subarray.java

--- 41_Maximum_Subarray.java
+++ 41_Maximum_Subarray.java
@@ -22,3 +22,54 @@ public class Solution {
         return maxSub;
     }
 }
+
+//solution 2: dynamic programming
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(int[] nums) {
+        // write your code
+        if (nums == null || nums.length==0){
+            return -1;
+        }
+        int maxSub = Integer.MIN_VALUE, minSub = 0, sum = 0;
+        // up to each index, keep track of the min sum
+        // the max value comes where sum thru i minus min sum so far
+        for (int i = 0; i < nums.length; i++){
+            minSub = Math.min(sum, minSub);
+            sum += nums[i];
+            maxSub = Math.max(maxSub, sum-minSub);
+        }
+        return maxSub;
+    }
+}
+
+//solution 3: dynamic programming
+
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A integer indicate the sum of max subarray
+     */
+    public int maxSubArray(ArrayList<Integer> nums) {
+        // -1 is not proper for illegal input
+        if (nums == null || nums.isEmpty()) return -1;
+
+        int size = nums.size();
+        int[] local = new int[size];
+        int[] global = new int[size];
+        local[0] = nums.get(0);
+        global[0] = nums.get(0);
+        for (int i = 1; i < size; i++) {
+            // drop local[i - 1] < 0
+            local[i] = Math.max(nums.get(i), local[i - 1] + nums.get(i));
+            // update global with local
+            global[i] = Math.max(global[i - 1], local[i]);
+        }
+
+        return global[size - 1];
+    }
+}
