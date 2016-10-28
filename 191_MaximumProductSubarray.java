07c2b0a111b01c4d648f88f0d41239898a3ec7aa
Alex Guo <qiuyuguo@usc.edu>
2016-10-28 07:20:11 -0700

    Update 191_MaximumProductSubarray.java

--- 191_MaximumProductSubarray.java
+++ 191_MaximumProductSubarray.java
@@ -10,6 +10,8 @@ public class Solution {
         int result = nums[0];
         max[0] = min[0] = nums[0];
         for (int i=1; i<nums.length; i++){
+            // if the previoius product is smaller than the current 
+            // item, then give up the previous product and start over
             max[i] = nums[i];
             min[i] = nums[i];
             if (nums[i] > 0){
