7b758e682501e4039b92709a7253b66717038982
Alex Guo <qiuyuguo@usc.edu>
2016-10-29 09:50:16 -0700

    Update 119_Edit_Distance.java

--- 119_Edit_Distance.java
+++ 119_Edit_Distance.java
@@ -9,6 +9,8 @@ public class Solution {
         int m = word2.length();
         int [][] stepCount = new int[n+1][m+1];
         
+        // initiate the step count matrix, value indicates number of steps to 
+        // match up to letter i of word1 and letter j of word2
         for (int i=0; i<n+1; i++){
             stepCount[i][0] = i;
         }
@@ -21,6 +23,10 @@ public class Solution {
                 if (word1.charAt(i-1) == word2.charAt(j-1)){
                     stepCount[i][j] = stepCount[i-1][j-1];
                 }
+                // in case of current letters not match choose from:
+                // [i-1][j-1]: replace
+                // [i-1][j]: insert word2[j] to word1
+                // [i][j-1]: delete word1[i]
                 else{
                     stepCount[i][j] = 1 + Math.min(stepCount[i-1][j-1], Math.min(stepCount[i-1][j], stepCount[i][j-1]));
                 }
